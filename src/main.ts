a739a33388e56792a5f77e2ae096c526d90ea3ec
mathias hansen <mathias@mathiass-MacBook-Pro.local>
2017-02-05 16:02:12 +0100

    added docker-compose

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,14 @@ import * as http from "http"
 import { readFileSync } from "fs"
 import { IncomingMessage, ServerResponse } from "http"
 
-interface Config {
-    host_port: number
-    proxies: { host: string, proxy: string }[]
-}
+type Config = { host: string | string[], proxy: string }[]
 
 const config: Config = JSON.parse(readFileSync(__dirname + "/../config.json", "utf8"))
 
-console.log(config)
+config
 
 http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    console.log(req.url)
-    console.log(req.method)
-    console.log(req.headers)
-
-    res.end()
-}).listen(config.host_port, () => {
-    console.log("listening http://localhost:" + config.host_port)
+    res.end(req.method + " http://" + req.headers["host"] + req.url)
+}).listen(8888, () => {
+    console.log("listening http://localhost/")
 })
