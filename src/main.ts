2f9b7ce909e7617e34d09485dc6dba6bd619c2e0
mathias hansen <mathias@mathiass-MacBook-Pro.local>
2017-02-05 16:17:39 +0100

    added proxy

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,44 @@ import * as http from "http"
 import { readFileSync } from "fs"
 import { IncomingMessage, ServerResponse } from "http"
 
-type Config = { host: string | string[], proxy: string }[]
+type Config = { host: string | string[], proxy: string }[]
 
 const config: Config = JSON.parse(readFileSync(__dirname + "/../config.json", "utf8"))
 
-config
-
 http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    res.end(req.method + " http://" + req.headers["host"] + req.url)
+    const method = req.method
+    const headers = req.headers
+    const host = headers["host"]
+    const path = req.url
+
+    const proxy = config.find(proxy => {
+        if (typeof proxy.host === "string") {
+            return proxy.host === host
+        }
+        else {
+            return proxy.host.includes(host)
+        }
+    })
+
+    if (proxy) {
+        const match = proxy.proxy.match(/(localhost|(?:\d+\.?){4})(?::(\d{1,4}))?/)
+
+        if (match) {
+            const [host, port] = match.slice(1)
+            const options = {
+                method,
+                host,
+                port: +port,
+                path
+            }
+
+            http.request(options, (r => r.pipe(res))).end()
+
+            return
+        }
+    }
+
+    res.end("ERROR")
 }).listen(8888, () => {
     console.log("listening http://localhost/")
 })
